f3b95723139b5da744ff79dcc057e24089f05d40
PavneetKaur15 <93992816+PavneetKaur15@users.noreply.github.com>
2022-02-08 13:30:52 +0530

    Update HelloController.java

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
-    String greeting = "Hello Jenkins";
+    String greeting = "Hello Jenkins?";
 
     @RequestMapping("/")
     public String index() {
